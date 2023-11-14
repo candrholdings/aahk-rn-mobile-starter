2730affd12af9f7de54de8126e53d87418511f30
joelicnr <joe.li@candrholdings.com>
2023-11-14 15:52:49 +0800

    refactor: lint

--- types/UIType.ts
+++ types/UIType.ts
@@ -1,44 +1,44 @@
-import { DrawerContentComponentProps } from "@react-navigation/drawer";
-import { ReactElement } from "react";
+import {DrawerContentComponentProps} from '@react-navigation/drawer';
+import {ReactElement} from 'react';
 
 export enum BUTTON_TYPE {
-    PRIMARY = 'PRIMARY',
-    DANGER = 'DANGER',
+  PRIMARY = 'PRIMARY',
+  DANGER = 'DANGER',
 }
 
 export enum BUTTON_MODE {
-    TEXT = 'text',
-    OUTLINED = 'outlined',
-    CONTAINED = 'contained',
-    ELEVATED = 'elevated',
-    CONTAINED_TONAL = 'contained-tonal'
+  TEXT = 'text',
+  OUTLINED = 'outlined',
+  CONTAINED = 'contained',
+  ELEVATED = 'elevated',
+  CONTAINED_TONAL = 'contained-tonal',
 }
 
 export enum PANEL_BUTTON_MODE {
-    OUTLINED = 'outlined',
-    CONTAINED = 'contained',
+  OUTLINED = 'outlined',
+  CONTAINED = 'contained',
 }
 
 export enum BUTTON_SIZE {
-    NORMAL = 'NORMAL',
-    LARGE = 'LARGE'
+  NORMAL = 'NORMAL',
+  LARGE = 'LARGE',
 }
 
 export enum RESULT_TYPE {
-    ERROR = 'ERROR',
-    SUCCEED = 'SUCCEED',
-    WARN = 'WARN',
-    INFO = 'INFO'
+  ERROR = 'ERROR',
+  SUCCEED = 'SUCCEED',
+  WARN = 'WARN',
+  INFO = 'INFO',
 }
 
 export enum INPUT_FIELD_MODE {
-    FLAT = 'flat',
-    OUTLINED = 'outlined'
+  FLAT = 'flat',
+  OUTLINED = 'outlined',
 }
 
 export enum SCAN_TYPE {
-    RACK_ID = 'RACK_ID',
-    BAG_TAG = 'BAG_TAG',
+  RACK_ID = 'RACK_ID',
+  BAG_TAG = 'BAG_TAG',
 }
 
 export const MODAL_ICON_SIZE = 240;
@@ -48,25 +48,26 @@ export const WARN_COLOR = '#FF8F00';
 export const ERROR_COLOR = '#E64A19';
 export const UNKNOWN_COLOR = '#9E9E9E';
 export interface IRouteScreen {
-    drawerLabel: string;
-    drawerIcon: ReactElement,
-    name: string;
-    component?: any;
-    screenOptions?: any;
-    initialParams?: any;
-    toScreenName: string;
-    isHide: boolean;
-    isScreen: boolean;
+  drawerLabel: string;
+  drawerIcon: ReactElement;
+  name: string;
+  component?: any;
+  screenOptions?: any;
+  initialParams?: any;
+  toScreenName: string;
+  isHide: boolean;
+  isScreen: boolean;
 }
 
 export interface IRouteConfig {
-    screens: IRouteScreen[];
-    isDrawer: boolean;
-    drawerContent: any;
-    initialRouteName: string;
-    homeRouteName: string;
+  screens: IRouteScreen[];
+  isDrawer: boolean;
+  drawerContent: any;
+  initialRouteName: string;
+  homeRouteName: string;
 }
 
-export interface ICustomDrawerContentComponentProps extends DrawerContentComponentProps {
-    config: IRouteConfig,
+export interface ICustomDrawerContentComponentProps
+  extends DrawerContentComponentProps {
+  config: IRouteConfig;
 }
